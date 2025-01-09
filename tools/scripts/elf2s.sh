469eba71a014174aa041705cf0ce5bff5cb57a59
MokhaLeee <nkulyc@163.com>
2025-01-09 12:31:34 +0000

    update symbol

--- tools/scripts/elf2s.sh
+++ tools/scripts/elf2s.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 tools/scripts/elf2s.py ~/fe11us-arm9/fe11-arm9.elf
+python3 tools/scripts/elf2s.py ~/fe11us-arm9/fe11-arm9.elf > asm/arm9.S
 python3 tools/scripts/elf2s-itcm.py ~/fe11us-arm9/itcm/itcm.elf > asm/itcm.S
