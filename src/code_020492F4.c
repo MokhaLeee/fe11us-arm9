38ccc0b58c03a4d1356db74c229b4c9667059f66
MokhaLeee <nkulyc@163.com>
2025-02-24 11:03:04 +0800

    decomp some functions

--- src/code_020492F4.c
+++ src/code_020492F4.c
@@ -1,13 +1,19 @@
 #include "global.h"
 
-struct UnkStruct_func_020492F4 {
-	int unk_00;
+struct UnkStruct_02196F0C;
+
+struct UnkStruct_02196F0C_00 {
+	void (*func)(struct UnkStruct_02196F0C *data, int index);
+};
+
+struct UnkStruct_02196F0C {
+	struct UnkStruct_02196F0C_00 *unk_00;
 	char **str_list;
-	int unk_08;
+	u8 *bytes;
 	int count;
 };
 
-int func_020492F4(struct UnkStruct_func_020492F4 *data, const char *str_ref)
+int Unk02196F0C_GetChunkIndexByName(struct UnkStruct_02196F0C *data, const char *str_ref)
 {
 	char **str = data->str_list;
 	int i = 0;
@@ -21,3 +27,68 @@ int func_020492F4(struct UnkStruct_func_020492F4 *data, const char *str_ref)
 	}
 	return -1;
 }
+
+BOOL Unk02196F0C_CheckBit(struct UnkStruct_02196F0C *data, u32 index)
+{
+	u8 *bytes = data->bytes;
+	int lo = index % 8;
+	int byte = bytes[index / 8];
+	int ref  = byte & (1 << lo);
+
+	if (ref & 0xFF)
+		return TRUE;
+
+	return FALSE;
+}
+
+BOOL Unk02196F0C_CheckBitByName(struct UnkStruct_02196F0C *data, const char *str_ref)
+{
+	int index = Unk02196F0C_GetChunkIndexByName(data, str_ref);
+
+	if (index == -1)
+		return FALSE;
+
+	return Unk02196F0C_CheckBit(data, index);
+}
+
+void Unk02196F0C_SetBit(struct UnkStruct_02196F0C *data, u32 index)
+{
+	u8 *bytes = data->bytes;
+	int lo = index % 8;
+	int byte = bytes[index / 8];
+
+	bytes[index / 8] = (byte | (1 << lo));
+}
+
+void Unk02196F0C_SetBitByName(struct UnkStruct_02196F0C *data, const char *str_ref)
+{
+	int index = Unk02196F0C_GetChunkIndexByName(data, str_ref);
+
+	if (index == -1)
+		return;
+
+	Unk02196F0C_SetBit(data, index);
+}
+
+void Unk02196F0C_ClearBit(struct UnkStruct_02196F0C *data, u32 index)
+{
+	u8 *bytes = data->bytes;
+	int lo = index % 8;
+	int tmp__1 = -1;
+	int byte = bytes[index / 8];
+
+	bytes[index / 8] = (tmp__1 ^ (1 << lo)) & byte;
+}
+
+#if 0
+void func_02049408(struct UnkStruct_02196F0C *data, const char *str_ref)
+{
+	int index = Unk02196F0C_GetChunkIndexByName(data, str_ref);
+
+	if (index != -1) {
+		struct UnkStruct_02196F0C *_data = data;
+
+		_data->unk_00->func(_data, index);
+	}
+}
+#endif
